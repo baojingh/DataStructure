da0331fb4f81ba61152de2f0eaa3273ff3a6b939
baojingh <baojingh@163.com>
2020-02-14 11:10:16 +0800

    binary  search 4 parts

--- src/main/java/binary_search/BinarySearchDemo.java
+++ src/main/java/binary_search/BinarySearchDemo.java
@@ -8,9 +8,9 @@ public class BinarySearchDemo {
 
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 3, 5, 7, 7, 8, 9};
-        int index = binarySearch1(arr, 2);
-        System.out.println("exists:" + index);
+        int[] arr = new int[]{1, 3, 5, 7, 9, 11, 13};
+        int index = binarySearch4(arr, 19);
+        System.out.println("exists: " + index);
     }
 
     /**
@@ -60,7 +60,71 @@ public class BinarySearchDemo {
             }
         }
         return -1;
-
     }
 
+    /**
+     * 查找最后一个等于指定值的元素，即数组中包含重复元素
+     */
+    public static int binarySearch2(int[] arr, int data) {
+        int low = 0;
+        int len = arr.length;
+        int high = len - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] > data) {
+                high = mid - 1;
+            } else if (arr[mid] < data) {
+                low = mid + 1;
+            } else {
+                if (mid == len - 1 || arr[mid + 1] != data) {
+                    return mid;
+                }
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找第一个大于等于指定值的元素
+     */
+    public static int binarySearch3(int[] arr, int data) {
+        int low = 0;
+        int len = arr.length;
+        int high = len - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] < data) {
+                low = mid + 1;
+            } else if (arr[mid] > data) {
+                // 若arr[mid]== data，则不会进入这个if条件，就会出现死循环
+                if (mid == 0 || arr[mid - 1] < data) {
+                    return mid;
+                }
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个小于等于指定值的元素
+     */
+    public static int binarySearch4(int[] arr, int data) {
+        int low = 0;
+        int len = arr.length;
+        int high = len - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] > data) {
+                high = mid - 1;
+            } else {
+                if (mid == len - 1 || arr[mid + 1] > data) {
+                    return mid;
+                }
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
