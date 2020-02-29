429396e361a47d825c0bb1ca41453a4dca7d93a5
baojingh <baojingh@163.com>
2020-02-29 19:53:35 +0800

    hashtable  resize get done

--- src/main/java/hashTable/HashTableCustomize.java
+++ src/main/java/hashTable/HashTableCustomize.java
@@ -50,19 +50,20 @@ public class HashTableCustomize<K, V> {
             tmp.next = n;
             size = size + 1;
             use = use + 1;
-            if (use >= table.length * LOAD_FACTOR) {
-                resize();
-            }
-        } else {
+//            if (use >= table.length * LOAD_FACTOR) {
+//                resize();
+//            }
+        } else if (tmp.next != null) {
             // 使用链表法解决冲突
-            do {
+            // 此处将do-while改写成while
+            while (tmp.next != null) {
                 tmp = tmp.next;
                 // 如果key相同，则覆盖
                 if (tmp.key == key) {
                     tmp.value = value;
                     return;
                 }
-            } while (tmp.next != null);
+            }
             // 获取原链表第一个节点
             EntryCustomize<K, V> temp = table[index].next;
             // 创建新的节点，插入表头
@@ -76,21 +77,25 @@ public class HashTableCustomize<K, V> {
         this.table = (EntryCustomize<K, V>[]) new EntryCustomize[this.table.length * 2];
         use = 0;
         for (int i = 0; i < oldTable.length; i++) {
-            if (oldTable[i] == null || oldTable[i].next == null) {
+            EntryCustomize<K, V> e = oldTable[i];
+            // 原数组中没有数据的，或者数据是(null,null,null)，就略过，不需要放入新数组
+            if (e == null || e.next == null) {
                 continue;
             }
-            EntryCustomize<K, V> e = oldTable[i];
             while (e.next != null) {
                 e = e.next;
                 int index = hash(e.key);
                 if (table[index] == null) {
+                    // 新数组index位置未被初始化
                     use = use + 1;
                     // 创建哨兵节点
                     table[index] = new EntryCustomize<>(null, null, null);
                 }
-                table[index].next = new EntryCustomize<>(e.key, e.value, table[index].next);
+                // 新数组中数组元素头节点元素
+                EntryCustomize<K, V> next = table[index].next;
+                // 新元素插入头部
+                table[index].next = new EntryCustomize<>(e.key, e.value, next);
             }
-
         }
     }
 
@@ -104,33 +109,38 @@ public class HashTableCustomize<K, V> {
         }
         EntryCustomize pre;
         EntryCustomize<K, V> headNode = table[index];
-        do {
+        while (e.next != null) {
             pre = e;
             e = e.next;
             if (e.key == key) {
                 pre.next = e.next;
                 size = size - 1;
+                // 删除链表节点后，此链表只剩下一个头节点(null,nuklk,null)
+                // use代表什么意思？
                 if (headNode.next == null) {
                     use = use - 1;
                 }
                 return;
             }
-        } while (e.next != null);
+        }
     }
 
     public V get(K key) {
         int index = hash(key);
         EntryCustomize<K, V> e = table[index];
-        // 为什么要求e.next == null
+        // e==null说明数组初始化完成，全部是null
+        // e.next == null 说明数组初始化成(null, null,next) 但是没有新的元素
         if (e == null || e.next == null) {
             return null;
         }
+        // 去链表中获取节点，真正的元素没有存储在数组中，均存在了以数据元素为头节点的链表中，因此
         while (e.next != null) {
             e = e.next;
             if (key == e.key) {
                 return e.value;
             }
         }
+        // 链表中不存在这个key
         return null;
     }
 
@@ -159,6 +169,9 @@ public class HashTableCustomize<K, V> {
         demo.put(5, "e");
         demo.put(6, "f");
         demo.put(7, "g");
+
+        String s = demo.get(3);
+        System.out.println(s);
     }
 
     static class EntryCustomize<K, V> {
