3881efe72ac47415077c2c5a7ff12685521dda22
baojingh <baojingh@163.com>
2020-03-09 09:18:36 +0800

    update: black and red tree give up

---
+++ src/main/java/tree/RedBlackTreeDemo.java
@@ -0,0 +1,19 @@
+package tree;
+
+/**
+ * @Author: hebj
+ * @Date: 2020/3/8 12:15
+ * @Description:
+ */
+public class RedBlackTreeDemo {
+
+    // 红黑树基础
+    // https://www.cnblogs.com/tiancai/p/9072813.html
+    // https://blog.csdn.net/abcdef314159/article/details/77193888
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
