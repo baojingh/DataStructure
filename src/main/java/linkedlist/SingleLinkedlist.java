2ab9f3bebce4dfcde4ddeb723f62ce6116022214
baojingh <baojingh@163.com>
2020-03-21 17:59:21 +0800

    update: odd and even element classify in linked list and little prove

--- src/main/java/linkedlist/SingleLinkedlist.java
+++ src/main/java/linkedlist/SingleLinkedlist.java
@@ -2,6 +2,8 @@ package linkedlist;
 
 import lombok.*;
 
+import javax.swing.tree.ExpandVetoException;
+
 /**
  * @Author: hebj
  * @Date: 2020/3/20 09:04
@@ -36,7 +38,7 @@ public class SingleLinkedlist {
     }
 
     /**
-     * 奇偶链表分别排列https://leetcode-cn.com/problems/odd-even-linked-list/
+     * 奇偶位置，链表分别排列https://leetcode-cn.com/problems/odd-even-linked-list/
      * 输入: 1->2->3->4->5->NULL
      * 输出: 1->3->5->2->4->NULL
      * <p>
@@ -45,7 +47,7 @@ public class SingleLinkedlist {
      *
      * @return
      */
-    public SingleNode oddEvenListClassify() {
+    public SingleNode oddEvenIndexListClassify() {
         if (head == null) {
             return null;
         }
@@ -58,12 +60,82 @@ public class SingleLinkedlist {
             SingleNode n = evenTail.getNext();
             oddTail.setNext(n);
             oddTail = oddTail.getNext();
+
             SingleNode n2 = oddTail.getNext();
             evenTail.setNext(n2);
             evenTail = evenTail.getNext();
         }
         oddTail.setNext(evenHead);
         return head;
+    }
+
+
+    /**
+     * 奇偶值，排列
+     * 输入: 1->2->3->4->5->NULL
+     * 输出: 1->3->5->2->4->NULL
+     * <p>
+     * 输入: 2->1->3->5->6->4->7->NULL
+     * 输出: 2->6->4->1->-3->5->NULL
+     * <p>
+     * 2020-03-21 此算法时间复杂度 O(N), 空间复杂度O(N)，空间复杂度高，这部分待优化
+     *
+     * @return
+     */
+    public SingleNode oddEvenValueListClassify() {
+        if (head == null) {
+            return null;
+        }
+        SingleNode solder = new SingleNode(null, 0);
+        solder.setNext(head);
+        // 链表首元素是偶数，就先排列偶数；反之亦然
+        SingleNode oddHead = null;
+        SingleNode oddTail = null;
+        SingleNode evenHead = null;
+        SingleNode evenTail = null;
+        SingleNode p = solder.getNext();
+        while (p != null) {
+            int val = p.getData();
+            SingleNode n = new SingleNode(null, val);
+            if ((val & 1) == 0) {
+                // 偶数
+                if (evenHead == null) {
+                    evenHead = n;
+                    evenTail = evenHead;
+                } else {
+                    evenTail.setNext(n);
+                    evenTail = evenTail.getNext();
+                }
+            } else {
+                // 奇数
+                if (oddHead == null) {
+                    oddHead = n;
+                    oddTail = oddHead;
+                } else {
+                    oddTail.setNext(n);
+                    oddTail = oddTail.getNext();
+                }
+            }
+            p = p.getNext();
+        }
+        if (oddHead != null) {
+            oddTail.setNext(evenHead);
+            head = oddHead;
+        } else {
+            evenTail.setNext(oddHead);
+            head = evenHead;
+        }
+        return null;
+    }
+
+    /**
+     * 是否是奇数
+     *
+     * @param val
+     * @return
+     */
+    private boolean isOdd(int val) {
+        return (val & 1) == 1;
     }
 
     public void printAll() {
@@ -78,22 +150,24 @@ public class SingleLinkedlist {
     public static void main(String[] args) {
         SingleLinkedlist linkedlist = new SingleLinkedlist();
         SingleNode node1 = new SingleNode(null, 2);
-//        SingleNode node2 = new SingleNode(null, 1);
-//        SingleNode node3 = new SingleNode(null, 3);
-//        SingleNode node4 = new SingleNode(null, 5);
-//        SingleNode node5 = new SingleNode(null, 6);
-//        SingleNode node6 = new SingleNode(null, 4);
-//        SingleNode node7 = new SingleNode(null, 7);
+        SingleNode node2 = new SingleNode(null, 4);
+        SingleNode node3 = new SingleNode(null, 3);
+        SingleNode node4 = new SingleNode(null, 5);
+        SingleNode node5 = new SingleNode(null, 6);
+        SingleNode node6 = new SingleNode(null, 9);
+        SingleNode node7 = new SingleNode(null, 7);
+        SingleNode node8 = new SingleNode(null, 8);
         linkedlist.insertToTail(node1);
-//        linkedlist.insertToTail(node2);
-//        linkedlist.insertToTail(node3);
-//        linkedlist.insertToTail(node4);
-//        linkedlist.insertToTail(node5);
-//        linkedlist.insertToTail(node6);
-//        linkedlist.insertToTail(node7);
+        linkedlist.insertToTail(node2);
+        linkedlist.insertToTail(node3);
+        linkedlist.insertToTail(node4);
+        linkedlist.insertToTail(node5);
+        linkedlist.insertToTail(node6);
+        linkedlist.insertToTail(node7);
+        linkedlist.insertToTail(node8);
         linkedlist.printAll();
 
-        linkedlist.oddEvenListClassify();
+        SingleNode node = linkedlist.oddEvenValueListClassify();
         linkedlist.printAll();
 
     }
