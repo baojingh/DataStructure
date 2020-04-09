9b5ed34d73dc1de31be8c58d9bdc0402e350dfe4
baojingh <baojingh@163.com>
2020-04-09 12:42:30 +0800

    three  number add with array

--- src/main/java/array/ArrayLeetcode.java
+++ src/main/java/array/ArrayLeetcode.java
@@ -1,6 +1,9 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @Author: hebj
@@ -18,6 +21,12 @@ public class ArrayLeetcode {
         System.out.println("array:");
         for (int i = 0; i < nums.length; i++) {
             System.out.println(nums[i]);
+        }
+    }
+
+    private void printList(List<List<Integer>> list) {
+        for (List<Integer> val : list) {
+            System.out.println(val);
         }
     }
 
@@ -102,15 +111,93 @@ public class ArrayLeetcode {
         return area;
     }
 
+    /**
+     * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+     * 注意：答案中不可以包含重复的三元组。
+     * 示例：
+     * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+     * 满足要求的三元组集合为：
+     * [
+     * [-1, 0, 1],
+     * [-1, -1, 2]
+     * ]
+     * 链接：https://leetcode-cn.com/problems/3sum
+     * 时间复杂度O
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        ArrayList<List<Integer>> arrayList = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return arrayList;
+        }
+        Arrays.sort(nums);
+        int len = nums.length;
+        // 此处不许i < len -1,因为while条件会验证L与R大小
+        for (int i = 0; i < len; i++) {
+            // 选取i对应的元素
+            int base = nums[i];
+            // base如果是正值，其后面的值肯定也是正值，因为数组被排序了。
+            // continue代表进行下一次循环
+            if (base > 0) {
+                // base大于0，后续树枝都会比base大，说明后续没有数字相加之和是0
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // 为base去重
+                continue;
+            }
+            // 比较区间[i+1, len - 1]
+            // 选区区间内的元素分别与base相加，是否是0
+            // 数组已排序，因此可以从两端向中间靠拢
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[L] + nums[R] + base;
+                if (sum == 0) {
+                    List<Integer> integerList = Arrays.asList(base, nums[L], nums[R]);
+                    arrayList.add(integerList);
+                    // 三数之和大于0，代表右侧数据比较大，右侧指针向左移动
+                    // 移动过程中发现两个相邻数据相等，就跳过
+                    // 此处为什么需要加 R > L ？ 应对[0,0,0]的情况
+                    while (R > L && nums[R] == nums[R - 1]) {
+                        R = R - 1;
+                    }
+                    // 三数之和小于0，代表左侧数据比较小，左侧指针向右移动
+                    // 移动过程中发现两个相邻数据相等，就跳过
+                    while (R > L && nums[L] == nums[L + 1]) {
+                        L = L + 1;
+                    }
+                    R = R - 1;
+                    L = L + 1;
+                } else if (sum > 0) {
+                    // 此处可以优化，如果与其左侧的值相等，就直接跳过
+                    /**
+                     while (nums[R] == nums[R - 1]) {
+                     R = R - 1;
+                     }
+                     */
+                    R = R - 1;
+                } else if (sum < 0) {
+                    // 优化方法参看上述信息
+                    L = L + 1;
+                }
+            }
+        }
+        return arrayList;
+    }
+
 
     public static void main(String[] args) {
         ArrayLeetcode array = new ArrayLeetcode();
-        int[] nums = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int[] nums = new int[]{};
 //        int target = 16;
 //        int[] ints = array.twoSum(nums, target);
 //        array.printAll(ints);
-        int i = array.maxArea(nums);
-        System.out.println(i);
-
+//        int i = array.maxArea(nums);
+//        System.out.println(i);
+        List<List<Integer>> lists = array.threeSum(nums);
+        array.printList(lists);
     }
 }
