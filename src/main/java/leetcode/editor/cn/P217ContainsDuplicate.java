4ffb25e648b4e69ee20933ed1cb8aac2f3da54b0
baojingh <baojingh@163.com>
2020-04-17 23:47:41 +0800

    add: repeat number with array

---
+++ src/main/java/leetcode/editor/cn/P217ContainsDuplicate.java
@@ -0,0 +1,52 @@
+//给定一个整数数组，判断是否存在重复元素。 
+//
+// 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。 
+// 示例 1:
+// 输入: [1,2,3,1]
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入: [1,2,3,4]
+//输出: false 
+//
+// 示例 3: 
+//
+// 输入: [1,1,1,3,3,4,3,2,4,2]
+//输出: true 
+// Related Topics 数组 哈希表
+
+
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+
+//Java：存在重复元素
+public class P217ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 2};
+        Solution solution = new P217ContainsDuplicate().new Solution();
+        boolean containsDuplicate = solution.containsDuplicate(nums);
+        System.out.println(containsDuplicate);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                Integer val = map.get(nums[i]);
+                // map中不存在此元素，则放入map
+                if (val == null) {
+                    map.put(nums[i], 1);
+                } else {
+                    // 如果map存在此元素，说明本次循环又拿到了这个值，即重复值
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
