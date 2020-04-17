de43d1924f331a2aab55eb11694592ffb7db8624
baojingh <baojingh@163.com>
2020-04-17 15:20:07 +0800

    add: target number with array

---
+++ src/main/java/leetcode/editor/cn/P167TwoSumIiInputArrayIsSorted.java
@@ -0,0 +1,56 @@
+//给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。 
+//
+// 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。 
+//
+// 说明: 
+//
+// 
+// 返回的下标值（index1 和 index2）不是从零开始的。 
+// 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。 
+// 
+//
+// 示例: 
+//
+// 输入: numbers = [2, 7, 11, 15], target = 9
+//输出: [1,2]
+//解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。 
+// Related Topics 数组 双指针 二分查找
+
+
+package leetcode.editor.cn;
+
+//Java：两数之和 II - 输入有序数组
+public class P167TwoSumIiInputArrayIsSorted {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,3,5,7,8};
+        Solution solution = new P167TwoSumIiInputArrayIsSorted().new Solution();
+        int[] ints = solution.twoSum(nums, 108);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i] + " ");
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int[] indexArray = new int[2];
+            int len = numbers.length;
+            int low = 0;
+            int high = len - 1;
+            for (int i = 0; i < len; i++) {
+                int sum = numbers[low] + numbers[high];
+                if (sum > target) {
+                    high = high - 1;
+                } else if (sum < target) {
+                    low = low + 1;
+                } else {
+                    indexArray[0] = low + 1;
+                    indexArray[1] = high + 1;
+                }
+            }
+            return indexArray;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
