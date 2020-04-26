5b7b8986fee46b209a384dc88cc12ab45ff0a9eb
baojingh <baojingh@163.com>
2020-04-26 18:51:18 +0800

    add with array with array

---
+++ src/main/java/leetcode/editor/cn/P989AddToArrayFormOfInteger.java
@@ -0,0 +1,106 @@
+//对于非负整数 X 而言，X 的数组形式是每位数字按从左到右的顺序形成的数组。例如，如果 X = 1231，那么其数组形式为 [1,2,3,1]。 
+//
+// 给定非负整数 X 的数组形式 A，返回整数 X+K 的数组形式。 
+// 示例 1：
+// 输入：A = [1,2,0,0], K = 34
+//输出：[1,2,3,4]
+//解释：1200 + 34 = 1234
+// 示例 2：
+// 输入：A = [2,7,4], K = 181
+//输出：[4,5,5]
+//解释：274 + 181 = 455
+//
+//
+// 示例 3：
+//
+// 输入：A = [2,1,5], K = 806
+//输出：[1,0,2,1]
+//解释：215 + 806 = 1021
+// 示例 4：
+//
+// 输入：A = [9,9,9,9,9,9,9,9,9,9], K = 1
+//输出：[1,0,0,0,0,0,0,0,0,0,0]
+//解释：9999999999 + 1 = 10000000000
+// 提示：
+//
+// 1 <= A.length <= 10000 
+// 0 <= A[i] <= 9 
+// 0 <= K <= 10000 
+// 如果 A.length > 1，那么 A[0] != 0 
+// 
+// Related Topics 数组
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//Java：数组形式的整数加法
+public class P989AddToArrayFormOfInteger {
+    public static void main(String[] args) {
+        int[] nums = new int[]{9, 9};
+        Solution solution = new P989AddToArrayFormOfInteger().new Solution();
+        List<Integer> list = solution.addToArrayForm(nums, 99);
+        for (int num : list) {
+            System.out.print(num + " ");
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<Integer> addToArrayForm(int[] A, int K) {
+
+            String num = K + "";
+            int kLen = num.length();
+            int[] kArray = new int[kLen];
+            int a = 0;
+            for (int j = kLen - 1; j >= 0; j--) {
+                a = K % 10;
+                K = K / 10;
+                kArray[j] = a;
+            }
+            List<Integer> list = new ArrayList<>();
+            int carry = 0;
+            int end = 0;
+            int sum = 0;
+            int aLen = A.length;
+            int i = aLen - 1;
+            int j = kLen - 1;
+            while (i >= 0 && j >= 0) {
+                sum = A[i] + kArray[j] + carry;
+                end = sum % 10;
+                carry = sum / 10;
+                list.add(end);
+                i = i - 1;
+                j = j - 1;
+            }
+
+            while (i >= 0) {
+                sum = A[i] + carry;
+                end = sum % 10;
+                carry = sum / 10;
+                list.add(end);
+                i = i - 1;
+            }
+
+
+            while (j >= 0) {
+                sum = kArray[j] + carry;
+                end = sum % 10;
+                carry = sum / 10;
+                list.add(end);
+                j = j - 1;
+            }
+            if (carry == 1) {
+                list.add(carry);
+            }
+
+            Collections.reverse(list);
+            return list;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
