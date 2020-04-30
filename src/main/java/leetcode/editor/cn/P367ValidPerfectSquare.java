41e4c141c52c720e3dabf4d7bdcecbb6d6027dff
baojingh <baojingh@163.com>
2020-04-30 14:45:01 +0800

    sqrt  with binary search

--- src/main/java/leetcode/editor/cn/P367ValidPerfectSquare.java
+++ src/main/java/leetcode/editor/cn/P367ValidPerfectSquare.java
@@ -22,13 +22,32 @@ public class P367ValidPerfectSquare {
     public static void main(String[] args) {
         String str = "abc";
         Solution solution = new P367ValidPerfectSquare().new Solution();
-
-        System.out.println();
+        boolean square = solution.isPerfectSquare(808201);
+        System.out.println(square);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean isPerfectSquare(int num) {
+            if (num < 2) {
+                return true;
+            }
+            int low = 2;
+            int high = num / 2;
+            int mid = 0;
+            long val = 0;
+            while (low <= high) {
+                mid = low + (high - low) / 2;
+                // 有mid*mid有溢出风险，转换成long
+                val = (long) mid * mid;
+                if (val > num) {
+                    high = mid - 1;
+                } else if (val < num) {
+                    low = mid + 1;
+                } else if (val == num) {
+                    return true;
+                }
+            }
             return false;
         }
     }
