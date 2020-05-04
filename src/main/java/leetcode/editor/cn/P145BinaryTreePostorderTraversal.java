8ad039561307ca9d76c3c6d5ea603e7d1bad63be
baojingh <baojingh@163.com>
2020-05-05 02:12:07 +0800

    postorder  with binary search tree

---
+++ src/main/java/leetcode/editor/cn/P145BinaryTreePostorderTraversal.java
@@ -0,0 +1,72 @@
+//给定一个二叉树，返回它的 后序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [3,2,1] 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树
+
+
+package leetcode.editor.cn;
+//Java：二叉树的后序遍历
+public class P145BinaryTreePostorderTraversal{
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node1.left = node2;
+        node1.right = node3;
+
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        n1.left = n2;
+        n1.right = n3;
+        Solution solution = new P145BinaryTreePostorderTraversal().new Solution();
+        
+        System.out.println();
+    }
+    
+   static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
