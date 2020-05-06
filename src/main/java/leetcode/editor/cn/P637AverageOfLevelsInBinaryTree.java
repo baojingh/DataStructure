d0d4ca471c0e2b27464f240b7abd3b275c041a6e
baojingh <baojingh@163.com>
2020-05-06 12:14:03 +0800

    bfs average  with binary search tree

---
+++ src/main/java/leetcode/editor/cn/P637AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,104 @@
+//给定一个非空二叉树, 返回一个由每层节点平均值组成的数组. 
+//
+// 示例 1: 
+//
+// 输入:
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//输出: [3, 14.5, 11]
+//解释:
+//第0层的平均值是 3,  第1层是 14.5, 第2层是 11. 因此返回 [3, 14.5, 11].
+// 
+//
+// 注意： 
+//
+// 
+// 节点值的范围在32位有符号整数范围内。 
+// 
+// Related Topics 树
+
+
+package leetcode.editor.cn;
+
+import java.util.LinkedList;
+import java.util.List;
+
+//Java：二叉树的层平均值
+public class P637AverageOfLevelsInBinaryTree {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node1.left = node2;
+        node1.right = node3;
+
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        n1.left = n2;
+        n1.right = n3;
+        Solution solution = new P637AverageOfLevelsInBinaryTree().new Solution();
+
+        System.out.println();
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public List<Double> averageOfLevels(TreeNode root) {
+            LinkedList<TreeNode> queue = new LinkedList<>();
+            LinkedList<Double> list = new LinkedList<>();
+            if (root == null) {
+                return list;
+            }
+            queue.offer(root);
+            while (queue.size() > 0) {
+                int size = queue.size();
+                int sum = 0;
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    sum = sum + node.val;
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                list.add(sum * 1.0 / size);
+            }
+            return list;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
