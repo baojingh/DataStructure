84bc27426c5883bd64f543d10b0d1ed8e516888e
baojingh <baojingh@163.com>
2020-05-07 14:25:29 +0800

    z output  with binary search tree

---
+++ src/main/java/leetcode/editor/cn/P103BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,111 @@
+//给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回锯齿形层次遍历如下： 
+//
+// [
+//  [3],
+//  [20,9],
+//  [15,7]
+//]
+// 
+// Related Topics 栈 树 广度优先搜索
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+//Java：二叉树的锯齿形层次遍历
+public class P103BinaryTreeZigzagLevelOrderTraversal {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node1.left = node2;
+        node1.right = node3;
+
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        n1.left = n2;
+        n1.right = n3;
+        Solution solution = new P103BinaryTreeZigzagLevelOrderTraversal().new Solution();
+
+        System.out.println();
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> list = new ArrayList<List<Integer>>();
+            if (root == null) {
+                return list;
+            }
+            LinkedList<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            int level = 0;
+            while (queue.size() > 0) {
+                level = level + 1;
+                int size = queue.size();
+                ArrayList<Integer> integers = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    if ((level & 1) == 1) {
+                        integers.add(node.val);
+                    } else {
+                        integers.add(0, node.val);
+                    }
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                list.add(integers);
+            }
+            return list;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
