faee1ed360e9de1655a345cd3909dbcbf9d16823
baojingh <baojingh@163.com>
2020-05-17 00:00:53 +0800

    design listnode

--- src/main/java/leetcode/editor/cn/P707DesignLinkedList.java
+++ src/main/java/leetcode/editor/cn/P707DesignLinkedList.java
@@ -38,13 +38,14 @@
 
 
 package leetcode.editor.cn;
+
 //Java：设计链表
-public class P707DesignLinkedList{
+public class P707DesignLinkedList {
     public static void main(String[] args) {
         MyLinkedList solution = new P707DesignLinkedList().new MyLinkedList();
         System.out.println();
     }
-    
+
     public class ListNode {
         int val;
         ListNode next;
@@ -52,44 +53,99 @@ public class P707DesignLinkedList{
         ListNode(int x) {
             val = x;
         }
-     }
-    
-    
-    //leetcode submit region begin(Prohibit modification and deletion)
-class MyLinkedList {
-
-    /** Initialize your data structure here. */
-    public MyLinkedList() {
-
     }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class MyLinkedList {
+
+        private ListNode head;
+        private ListNode solder;
+        private int size;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyLinkedList() {
+            solder = new ListNode(-1);
+            size = 0;
+        }
+
+        private int size() {
+            return size;
+        }
 
         /**
          * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
          */
         public int get(int index) {
-            return 0;
+            if(index < 0 || index >= size)
+                return -1;
+            ListNode curr = solder.next;
+            for(int i = 0; i < index; i++)
+                curr = curr.next;
+            return curr.val;
         }
-    
-    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-    public void addAtHead(int val) {
 
-    }
-    
-    /** Append a node of value val to the last element of the linked list. */
-    public void addAtTail(int val) {
+        /**
+         * Add a node of value val before the first element of the linked list. After the insertion,
+         * the new node will be the first node of the linked list.
+         */
+        public void addAtHead(int val) {
+            ListNode newNode = new ListNode(val);
+            newNode.next = head;
+            size = size + 1;
+            head = newNode;
+        }
 
-    }
-    
-    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-    public void addAtIndex(int index, int val) {
+        /**
+         * Append a node of value val to the last element of the linked list.
+         */
+        public void addAtTail(int val) {
+            addAtIndex(size, val);
+        }
 
-    }
-    
-    /** Delete the index-th node in the linked list, if the index is valid. */
-    public void deleteAtIndex(int index) {
+        /**
+         * Add a node of value val before the index-th node in the linked list.
+         * If index equals to the length of linked list, the node will be appended to the end of linked list.
+         * If index is greater than the length, the node will not be inserted.
+         */
+        public void addAtIndex(int index, int val) {
+            if (index > size) {
+                return;
+            }
+            if (index < 0) {
+                addAtHead(val);
+                return;
+            }
 
+            ListNode prev = solder;
+            for (int i = 0; i < index; i++) {
+                prev = prev.next;
+            }
+            ListNode newNode = new ListNode(val);
+            newNode.next = prev.next;
+            prev.next = newNode;
+            size = size + 1;
+        }
+
+        /**
+         * Delete the index-th node in the linked list, if the index is valid.
+         */
+        public void deleteAtIndex(int index) {
+            if (index >= size || index < 0) {
+                return;
+            }
+            ListNode prev = solder;
+            for (int i = 0; i < index; i++) {
+                prev = prev.next;
+            }
+            ListNode n = prev.next;
+            prev.next = n.next;
+            n.next = null;
+            size = size - 1;
+        }
     }
-}
 
 /**
  * Your MyLinkedList object will be instantiated and called as such:
