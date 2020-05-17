575cf11cd4948df20486e1ae1a554badd2cd960d
baojingh <baojingh@163.com>
2020-05-17 10:10:23 +0800

    is cycle with linked list

---
+++ src/main/java/entity/ListNode.java
@@ -0,0 +1,16 @@
+package entity;
+
+/**
+ * @Author: hebj
+ * @Date: 2020/5/17 09:33
+ * @Description:
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+}
