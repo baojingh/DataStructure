0a180fb90939db960d9ac9ac8f3bc4ef0fa7e52b
baojingh <baojingh@163.com>
2020-05-21 21:41:45 +0800

    remove kth summary  with linked list

---
+++ src/main/java/leetcode/editor/cn/P24SwapNodesInPairs.java
@@ -0,0 +1,50 @@
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 1->2->3->4, 你应该返回 2->1->4->3.
+// 
+// Related Topics 链表
+
+
+package leetcode.editor.cn;
+
+//Java：两两交换链表中的节点
+public class P24SwapNodesInPairs {
+    public static void main(String[] args) {
+        Solution solution = new P24SwapNodesInPairs().new Solution();
+        System.out.println();
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
