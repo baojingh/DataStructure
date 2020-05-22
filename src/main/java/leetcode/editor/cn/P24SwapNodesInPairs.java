0eceba331a222b0f5f4d27f409e887593760eb2c
baojingh <baojingh@163.com>
2020-05-22 23:42:43 +0800

    swap node with linked list

--- src/main/java/leetcode/editor/cn/P24SwapNodesInPairs.java
+++ src/main/java/leetcode/editor/cn/P24SwapNodesInPairs.java
@@ -17,6 +17,18 @@ package leetcode.editor.cn;
 public class P24SwapNodesInPairs {
     public static void main(String[] args) {
         Solution solution = new P24SwapNodesInPairs().new Solution();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        ListNode solder = new ListNode(-1);
+        solder.next = head;
+        ListNode listNode = solution.swapPairs(head);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+
         System.out.println();
     }
 
@@ -41,8 +53,38 @@ public class P24SwapNodesInPairs {
      * }
      */
     class Solution {
+        /**
+         * 解释题意
+         * 1 两两交换的意思是相邻两个元素交换，但是如果这个元素已经交换过，就不参与交换
+         * 2
+         *
+         * 设计算法
+         * 测试用例
+         * 1 空链表，-1-null
+         * 2 -1-1-null
+         * 3 -1-1-2-null
+         * 4 -1-1-2-3-null
+         * 5 -1-1-2-3-4-null
+         *
+         * @param head
+         * @return
+         */
         public ListNode swapPairs(ListNode head) {
-
+            ListNode solder = new ListNode(-1);
+            solder.next = head;
+            ListNode cur = solder;
+            while (cur.next != null && cur.next.next != null) {
+                // 当前节点是cur，找到下一个节点以及下下一个节点
+                ListNode tmp1 = cur.next;
+                ListNode tmp2 = cur.next.next;
+                // 最右边的节点设置next
+                tmp1.next = tmp2.next;
+                tmp2.next = tmp1;
+                cur.next = tmp2;
+                // 要交换的两个节点的前一个节点
+                cur = cur.next.next;
+            }
+            return solder.next;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
