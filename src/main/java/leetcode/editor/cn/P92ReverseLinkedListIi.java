378e4e5fe8394b1444546e516ebe81f686849535
baojingh <baojingh@163.com>
2020-06-02 14:16:15 +0800

    reverse 2 comment  with linked list

--- src/main/java/leetcode/editor/cn/P92ReverseLinkedListIi.java
+++ src/main/java/leetcode/editor/cn/P92ReverseLinkedListIi.java
@@ -52,7 +52,7 @@ public class P92ReverseLinkedListIi {
          * 2 按照链表反转的流程进行反转，注意从第m个位置到第n个位置
          * 3 https://leetcode-cn.com/problems/reverse-linked-list-ii/solution/ji-bai-liao-100de-javayong-hu-by-reedfan-6/
          * 4 目标链表反转完毕，开始做拼接：cur是目标链表之后的第一个节点，应将cur赋值给目标链表的第一个节点的next；
-         * prev的下一个节点是目标链表的最后一个节点
+         * 5 prev的下一个节点是目标链表的最后一个节点
          *
          *
          *
@@ -71,22 +71,32 @@ public class P92ReverseLinkedListIi {
             if (m < 1 || m > n) {
                 return head;
             }
+            // 创建solder节点
             ListNode solder = new ListNode(-1);
             solder.next = head;
             ListNode prev = solder;
             for (int i = 1; i < m; i++) {
                 prev = prev.next;
             }
+            // prev是目标节点的前一个节点
             ListNode cur = prev.next;
+            // 待反转的链表的前置节点
             ListNode p = null;
+            // 待反转的链表的后续节点
             ListNode next = null;
+            // 要反转的有几个节点就迭代多少次
             for (int i = m; i < n + 1; i++) {
+                // 现获取当前节点的后续节点
                 next = cur.next;
+                // 当前节点的后续节点指向p
                 cur.next = p;
+                // 移动两个变量位置成为新的p，cur
                 p = cur;
                 cur = next;
             }
+            // 待反转的链表头指向待反转链表的后面节点
             prev.next.next = cur;
+            // 前置节点prev指向待反转链表最后一个节点
             prev.next = p;
             return solder.next;
         }
