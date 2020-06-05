80ab2fd8380f7130c1880efb8218bff1045c2100
baojingh <baojingh@163.com>
2020-06-05 09:24:29 +0800

    odd and even with linked list

--- src/main/java/leetcode/editor/cn/P328OddEvenLinkedList.java
+++ src/main/java/leetcode/editor/cn/P328OddEvenLinkedList.java
@@ -74,7 +74,11 @@ public class P328OddEvenLinkedList {
          * <p>
          * 测试用例
          * 1 null
-         * 2
+         * 2 1
+         * 3 1-2
+         * 4 1-2-3
+         * 5 1-2-3-4
+         * 6 1-2-3-4-5
          *
          * @param head
          * @return
