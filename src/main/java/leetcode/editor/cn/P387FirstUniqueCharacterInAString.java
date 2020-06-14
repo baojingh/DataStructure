0487a9f4d7aa97ae115d43e1b7807fb6c9d1997f
baojingh <baojingh@163.com>
2020-06-14 16:34:06 +0800

    first unique with hash

---
+++ src/main/java/leetcode/editor/cn/P387FirstUniqueCharacterInAString.java
@@ -0,0 +1,36 @@
+//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。 
+//
+// 
+//
+// 示例： 
+//
+// s = "leetcode"
+//返回 0
+//
+//s = "loveleetcode"
+//返回 2
+// 
+//
+// 
+//
+// 提示：你可以假定该字符串只包含小写字母。 
+// Related Topics 哈希表 字符串
+
+
+package leetcode.editor.cn;
+//Java：字符串中的第一个唯一字符
+public class P387FirstUniqueCharacterInAString{
+    public static void main(String[] args) {
+        Solution solution = new P387FirstUniqueCharacterInAString().new Solution();
+        System.out.println();
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int firstUniqChar(String s) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
