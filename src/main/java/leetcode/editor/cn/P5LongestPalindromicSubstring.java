72e91fbf715d17995bb981da6ff300935ee3cdb5
baojingh <baojingh@163.com>
2020-06-23 22:25:15 +0800

    palindrome  with dp

---
+++ src/main/java/leetcode/editor/cn/P5LongestPalindromicSubstring.java
@@ -0,0 +1,70 @@
+//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。 
+//
+// 示例 1： 
+//
+// 输入: "babad"
+//输出: "bab"
+//注意: "aba" 也是一个有效答案。
+// 
+//
+// 示例 2： 
+//
+// 输入: "cbbd"
+//输出: "bb"
+// 
+// Related Topics 字符串 动态规划
+
+
+package leetcode.editor.cn;
+
+//Java：最长回文子串
+public class P5LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution solution = new P5LongestPalindromicSubstring().new Solution();
+        System.out.println();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestPalindrome(String s) {
+            // 特判
+            int len = s.length();
+            if (len < 2) {
+                return s;
+            }
+
+            int maxLen = 1;
+            int begin = 0;
+
+            // dp[i][j] 表示 s[i, j] 是否是回文串
+            boolean[][] dp = new boolean[len][len];
+            char[] charArray = s.toCharArray();
+
+            for (int i = 0; i < len; i++) {
+                dp[i][i] = true;
+            }
+            for (int j = 1; j < len; j++) {
+                for (int i = 0; i < j; i++) {
+                    if (charArray[i] != charArray[j]) {
+                        dp[i][j] = false;
+                    } else {
+                        if (j - i < 3) {
+                            dp[i][j] = true;
+                        } else {
+                            dp[i][j] = dp[i + 1][j - 1];
+                        }
+                    }
+
+                    // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
+                    if (dp[i][j] && j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        begin = i;
+                    }
+                }
+            }
+            return s.substring(begin, begin + maxLen);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
