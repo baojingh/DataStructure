5328aba4a55009858eb13679647bce2116112d04
baojingh <baojingh@163.com>
2020-07-11 21:40:56 +0800

    palindrome  with math

---
+++ src/main/java/leetcode/editor/cn/P9PalindromeNumber.java
@@ -0,0 +1,105 @@
+//判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 
+//
+// 示例 1: 
+//
+// 输入: 121
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: -121
+//输出: false
+//解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+// 
+//
+// 示例 3: 
+//
+// 输入: 10
+//输出: false
+//解释: 从右向左读, 为 01 。因此它不是一个回文数。
+// 
+//
+// 进阶: 
+//
+// 你能不将整数转为字符串来解决这个问题吗？ 
+// Related Topics 数学 
+// 👍 1143 👎 0
+
+
+package leetcode.editor.cn;
+
+//Java：回文数
+public class P9PalindromeNumber {
+    public static void main(String[] args) {
+        Solution solution = new P9PalindromeNumber().new Solution();
+        System.out.println();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isPalindrome(int x) {
+            if (x < 0) {
+                return false;
+            }
+            int res = 0;
+            int tmp = 0;
+            int buf = x;
+            while (x != 0) {
+                tmp = x % 10;
+                res = res * 10 + tmp;
+                x = x / 10;
+            }
+            return res == buf;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
