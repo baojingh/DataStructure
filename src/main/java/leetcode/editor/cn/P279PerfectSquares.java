4c06f814790ee231a682a7dd7df9666204403a55
baojingh <baojingh@163.com>
2020-07-17 11:17:50 +0800

    pow 3  with math

---
+++ src/main/java/leetcode/editor/cn/P279PerfectSquares.java
@@ -0,0 +1,36 @@
+//给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。 
+//
+// 示例 1: 
+//
+// 输入: n = 12
+//输出: 3 
+//解释: 12 = 4 + 4 + 4. 
+//
+// 示例 2: 
+//
+// 输入: n = 13
+//输出: 2
+//解释: 13 = 4 + 9. 
+// Related Topics 广度优先搜索 数学 动态规划 
+// 👍 494 👎 0
+
+
+package leetcode.editor.cn;
+
+//Java：完全平方数
+public class P279PerfectSquares {
+    public static void main(String[] args) {
+        Solution solution = new P279PerfectSquares().new Solution();
+        System.out.println();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numSquares(int n) {
+
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/leetcode/editor/cn/P326PowerOfThree.java
@@ -0,0 +1,80 @@
+//给定一个整数，写一个函数来判断它是否是 3 的幂次方。 
+//
+// 示例 1: 
+//
+// 输入: 27
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: 0
+//输出: false 
+//
+// 示例 3: 
+//
+// 输入: 9
+//输出: true 
+//
+// 示例 4: 
+//
+// 输入: 45
+//输出: false 
+//
+// 进阶： 
+//你能不使用循环或者递归来完成本题吗？ 
+// Related Topics 数学 
+// 👍 116 👎 0
+
+
+package leetcode.editor.cn;
+
+//Java：3的幂
+public class P326PowerOfThree {
+    public static void main(String[] args) {
+        Solution solution = new P326PowerOfThree().new Solution();
+        System.out.println();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isPowerOfThree(int n) {
+            if (n < 1) {
+                return false;
+            }
+            while (n % 3 == 0) {
+                n = n / 3;
+            }
+            if (n == 1) {
+                return true;
+            }
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
