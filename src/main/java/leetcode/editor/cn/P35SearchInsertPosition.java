0aabc46fe068a1ece89fcbab8666895e9441b822
baojingh <baojingh@163.com>
2020-09-02 19:54:33 +0800

    location with bs  with hashmap

--- src/main/java/leetcode/editor/cn/P35SearchInsertPosition.java
+++ src/main/java/leetcode/editor/cn/P35SearchInsertPosition.java
@@ -26,7 +26,66 @@ public class P35SearchInsertPosition {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        /**
+         * 设计算法
+         * 1 二分查找目标元素，如果找到了返回索引值
+         * 2 同时使用两个变量存储最近两次的中间位置mid1,mid2
+         * 3 没有找到就将目标值查到mid1,mid2中间位置
+         *
+         * 测试用例
+         * 1 1,3,5,7 5
+         * 1 1,3,5,7 1
+         * 1 1,3,5,7 7
+         * 2 1,3,5 0
+         * 2 1,3,5 6
+         * 2 1,3,5 4
+         *
+         * 或者归纳一下，可知返回的是low
+         * 1,3,5,6 7
+         * 1,3,5,6 0
+         * 1,3,5,6 4
+         *
+         *
+         * @param nums
+         * @param target
+         * @return
+         */
         public int searchInsert(int[] nums, int target) {
+            int low = 0;
+            int high = nums.length - 1;
+            int mid = 0;
+
+            while (low <= high) {
+                mid = low + (high - low) / 2;
+                if (nums[mid] < target) {
+                    low = mid + 1;
+                } else if (nums[mid] > target) {
+                    high = mid - 1;
+                } else {
+                    return mid;
+                }
+            }
+            // 运行到这里，说明没有找到目标值
+            return low;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public int searchInsert1(int[] nums, int target) {
             // 模版1，low是0；high是length
             int low = 0;
             int high = nums.length;
