05d1f3db4074a01b0ed3314982582a9fdb4af7cb
baojingh <baojingh@163.com>
2020-09-20 09:45:51 +0800

    repeat 2 code  with array

--- src/main/java/leetcode/editor/cn/P219ContainsDuplicateIi.java
+++ src/main/java/leetcode/editor/cn/P219ContainsDuplicateIi.java
@@ -29,34 +29,38 @@ public class P219ContainsDuplicateIi {
     class Solution {
 
         /**
-         *
          * 设计算法
          * 1 使用hash表存储数组元素以及相应的索引
-         * 2 如果元素已存在，比较两个索引是否满足条件
+         * 2 如果不存在，舅加入map
+         * 2 如果元素已存在，比较两个索引是否满足条件，满足就返回，不满足需要更新元素在map中的值，处理(1,0,1,1 1)的情况
          * 3 迭代结束条件是元素是否已迭代完成
-         *
+         * <p>
          * 测试用例
          * 1 1,2,3 2
-         * 2 1,1 1
+         * 2 1,1,1 1
          * 3 1,1 2
          * 4 1 2
          * 5 1,2,4,2 2
-         *
          *
          * @param nums
          * @param k
          * @return
          */
-        public boolean containsNearbyDuplicate1(int[] nums, int k) {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                Integer integer = map.get(nums[i]);
+                if (integer != null) {
+                    int gap = Math.abs(integer - i);
+                    if (gap <= k) {
+                        return true;
+                    }
+                }
+                // 更新重复元素在map中的kv
+                map.put(nums[i], i);
+            }
             return false;
         }
-
-
-
-
-
-
-
 
 
 
@@ -108,7 +112,7 @@ public class P219ContainsDuplicateIi {
          * @param k
          * @return
          */
-        public boolean containsNearbyDuplicate(int[] nums, int k) {
+        public boolean containsNearbyDuplicate2(int[] nums, int k) {
             if (nums == null || k < 1) {
                 return false;
             }
