4cbf88a0dadcd8d1d1d28881bfb4a5ede69bb455
baojingh <baojingh@163.com>
2020-09-23 10:50:12 +0800

    move 0 code    with array

--- src/main/java/leetcode/editor/cn/P283MoveZeroes.java
+++ src/main/java/leetcode/editor/cn/P283MoveZeroes.java
@@ -23,7 +23,63 @@ public class P283MoveZeroes {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
+
+        /**
+         * 设想
+         * 1 遍历数组，确定0的个数
+         * 2 再次迭代将各个非零元素
+         * <p>
+         * <p>
+         * 设计算法
+         * 1
+         * <p>
+         * <p>
+         * 测试用例
+         * 1 0,2,3
+         * 1 0,2,0,3,0
+         * 1 1,2,0,3,4,0,5
+         * 2 1,2,3
+         * 3 1
+         * 4 0
+         * 5 0,0
+         * 6 1,0,0
+         * 7
+         *
+         * @param nums
+         */
         public void moveZeroes(int[] nums) {
+            int index = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    nums[index] = nums[i];
+                    index = index + 1;
+                }
+            }
+            for (int i = index; i < nums.length; i++) {
+                nums[i] = 0;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void moveZeroes1(int[] nums) {
             int index = 0;
             for (int i = 0; i < nums.length; i++) {
                 if (nums[i] != 0) {
