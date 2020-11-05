a47d29b24564065e5cea2c0d279652049824b10f
baojingh <baojingh@163.com>
2020-11-05 20:20:04 +0800

    2 pow need improve   with math

--- src/main/java/leetcode/editor/cn/P231PowerOfTwo.java
+++ src/main/java/leetcode/editor/cn/P231PowerOfTwo.java
@@ -44,7 +44,15 @@ public class P231PowerOfTwo {
          * @return
          */
         public boolean isPowerOfTwo(int n) {
-
+            if (n < 0) {
+                return false;
+            }
+            while (n % 2 == 0) {
+                n = n / 2;
+            }
+            if (n == 1) {
+                return true;
+            }
             return false;
         }
 
