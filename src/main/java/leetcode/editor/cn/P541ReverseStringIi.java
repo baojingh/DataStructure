80277d1f438ec523b6fafab6bda09f3da58ab7c3
baojingh <baojingh@163.com>
2021-06-21 21:48:48 +0800

    add:  diameter of bt

---
+++ src/main/java/leetcode/editor/cn/P541ReverseStringIi.java
@@ -0,0 +1,46 @@
+//给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。 
+//
+// 
+// 如果剩余字符少于 k 个，则将剩余字符全部反转。 
+// 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。 
+// 
+//
+// 
+//
+// 示例: 
+//
+// 输入: s = "abcdefg", k = 2
+//输出: "bacdfeg"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 该字符串只包含小写英文字母。 
+// 给定字符串的长度和 k 在 [1, 10000] 范围内。 
+// 
+// Related Topics 字符串 
+// 👍 127 👎 0
+
+
+package leetcode.editor.cn;
+
+//Java：反转字符串 II
+public class P541ReverseStringIi {
+    //public class JianZhiReverseStringIi{
+    public static void main(String[] args) {
+        Solution solution = new P541ReverseStringIi().new Solution();
+        System.out.println();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String reverseStr(String s, int k) {
+            return "";
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/leetcode/editor/cn/P543DiameterOfBinaryTree.java
@@ -0,0 +1,106 @@
+//给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。 
+//
+// 
+//
+// 示例 : 
+//给定二叉树 
+//
+//           1
+//         / \
+//        2   3
+//       / \     
+//      4   5    
+// 
+//
+// 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。 
+//
+// 
+//
+// 注意：两结点之间的路径长度是以它们之间边的数目表示。 
+// Related Topics 树 
+// 👍 722 👎 0
+
+
+package leetcode.editor.cn;
+
+//Java：二叉树的直径
+public class P543DiameterOfBinaryTree {
+    //public class JianZhiDiameterOfBinaryTree{
+    public static void main(String[] args) {
+        Solution solution = new P543DiameterOfBinaryTree().new Solution();
+        System.out.println();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+
+    public class TreeNode {
+        int val;
+        boolean isVisited;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int diameterOfBinaryTree(TreeNode root) {
+
+
+
+
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
